660288cc4d3e58521b7e4912394bb4792bbc93a5
Chidi Williams <williamschidi1@gmail.com>
2022-09-08 08:54:43 +0100

    Primitives

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,63 @@ use std::{
 };
 
 fn main() {
+    primitives();
+}
+
+fn primitives() {
+    let logical: bool = true;
+
+    let a_float: f64 = 1.0;
+    let an_integer = 5i32;
+
+    let default_float = 3.0;
+    let default_integer = 7;
+
+    let mut inferred_type = 12;
+    inferred_type = 4294967296i64;
+
+    let mut mutable = 12;
+    mutable = 21;
+
+    // mutable = true;
+
+    let mutable = true;
+
+    let long_tuple = (1u8, -2i16, 'a', true, 0.2f64);
+
+    println!("first value: {}", long_tuple.0);
+
+    let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
+
+    let too_long_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
+    // println!("too long tuple: {:?}", too_long_tuple);
+
+    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+    println!("{:?}", matrix);
+    println!("{}", matrix);
+
+    println!("{}", transpose(matrix))
+}
+
+fn reverse(pair: (i32, bool)) -> (bool, i32) {
+    let (integer, boolean) = pair;
+    (boolean, integer)
+}
+
+fn transpose(matrix: Matrix) -> Matrix {
+    Matrix(matrix.0, matrix.2, matrix.1, matrix.3)
+}
+
+#[derive(Debug)]
+struct Matrix(f32, f32, f32, f32);
+
+impl fmt::Display for Matrix {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "( {} {} )\n( {} {} )", self.0, self.1, self.2, self.3)
+    }
+}
+
+fn hello_world() {
     #[derive(Debug)]
     struct Structure(i32);
 
